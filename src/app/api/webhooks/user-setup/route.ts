6c17040cc0cc105d1e67b70563d88a369efc5b56
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-03-15 12:19:24 +0500

    Database setup + webhook for user setup

---
+++ src/app/api/webhooks/user-setup/route.ts
@@ -0,0 +1,53 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const data = body.data
+    try {
+        switch (body.type) {
+            case "user.created":
+                await prisma.user.create({
+                    data: {
+                        userId: data.id,
+                        firstName: data.first_name,
+                        lastName: data.last_name,
+                        avatar: data.profile_image_url,
+
+                        username: data.first_name,
+                        email: data.email_addresses[0].email_address,
+                    },
+                });
+
+                return NextResponse.json('User Successfully Created', { status: 200 });
+
+            case 'user.updated':
+                await prisma.user.update({
+                    where: {
+                        userId: data.id
+                    },
+                    data: {
+                        username: data.username,
+                        firstName: data.first_name,
+                        lastName: data.last_name,
+                        avatar: data.profile_image_url,
+
+                        email: data.email_addresses[0].email_address,
+                    },
+                });
+                return NextResponse.json('User Successfully Updated', { status: 200 });
+
+            case 'user.deleted':
+                await prisma.user.delete({
+                    where: {
+                        userId: data.id
+                    },
+                });
+                return NextResponse.json('User Successfully Deleted', { status: 200 });
+        };
+    } catch (error) {
+        return NextResponse.json('Failed to create user' + error, { status: 200 });
+    };
+
+    return NextResponse.json({ message: "Success", status: 200 });
+};
