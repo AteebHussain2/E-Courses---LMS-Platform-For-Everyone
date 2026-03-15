bca25d89c0a3db8e21d0e4b307357bbbe045e6a5
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-03-15 12:56:14 +0500

    Grouped Routes and Home Page

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+    title: "ECourses - Home",
+    description: "A platform for online courses and learning resources.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <main>
+                {children}
+            </main>
+        </div>
+    );
+}
