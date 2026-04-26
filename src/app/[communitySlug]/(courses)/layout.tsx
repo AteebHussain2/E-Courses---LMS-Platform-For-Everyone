772eebb12763cb2f02ceb12e3b5e60c9c64f417d
AteebHussain2 <student2ateeb.hussain@gmail.com>
2026-04-26 22:05:38 +0500

    layout changes for course

--- src/app/[communitySlug]/(courses)/layout.tsx
+++ src/app/[communitySlug]/(courses)/layout.tsx
@@ -1,7 +1,36 @@
-export default async function LessonLayout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getCommunitySEO } from "@/lib/seo";
+import HomeSidebar from "@/components/sidebar/HomeSidebar";
+import { Role } from "@/generated/prisma/enums";
+
+type Props = {
+    params: Promise<{ communitySlug: string }>;
+    children: React.ReactNode;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const data = await getCommunitySEO((await params).communitySlug);
+    return data?.metadata ?? { title: "Community" };
+}
+
+export default async function RootLayout({ params, children }: Props) {
+    const { communitySlug } = await params;
+    const seoData = await getCommunitySEO(communitySlug);
+
+    if (!seoData || !seoData?.community) return notFound();
+
     return (
-        <div>
-            {children}
+        <div className="max-h-screen flex flex-row flex-1 items-start justify-center">
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{ __html: JSON.stringify(seoData.jsonLd) }}
+            />
+            <div className="relative w-full max-h-screen mt-3 mx-3">
+                <main className="overflow-clip space-y-5 bg-background w-full min-h-[calc(100vh-12px)] h-full border-x border-t border-border rounded-t-[20px]">
+                    {children}
+                </main>
+            </div>
         </div>
-    )
+    );
 }
